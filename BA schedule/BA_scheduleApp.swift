5d201abefff6dfa699448218d61262db41602fb9
Jonas Richard Richter <kontakt@jonasrichter.eu>
2021-03-20 20:00:37 +0100

    Initial Commit

---
+++ BA schedule/BA_scheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BA_scheduleApp.swift
+//  BA schedule
+//
+//  Created by Jonas Richard Richter on 20.03.21.
+//
+
+import SwiftUI
+
+@main
+struct BA_scheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
